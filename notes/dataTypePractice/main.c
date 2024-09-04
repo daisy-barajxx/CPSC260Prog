42f038c981460e4a3c4d771adaf401ff6dfe4eeb
DB <164849132+daisy-barajxx@users.noreply.github.com>
2024-09-04 04:36:36 +0000

    initial commit

---
+++ notes/dataTypePractice/main.c
@@ -0,0 +1,12 @@
+// C language
+#include<stdio.h>
+#include<stdbool.h>
+
+int main() 
+{
+  // int size = sizeof(char);
+  // printf("char is: %d bytes\n", size); // print an integer (replace %d ) 
+  double num = 3.141592653;
+  printf("%f\n", num);
+  return 0;
+}
